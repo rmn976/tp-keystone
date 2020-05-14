0de8924de7e64e8ec5a2ba4117709e717c58ad97
rmn976 <rmn976dev@gmail.com>
2020-05-14 11:25:57 +0200

    Commit initial

---
+++ index.js
@@ -0,0 +1,140 @@
+const { Keystone } = require('@keystonejs/keystone');
+const { PasswordAuthStrategy } = require('@keystonejs/auth-password');
+const { Text, Checkbox, Password, File } = require('@keystonejs/fields');
+const { GraphQLApp } = require('@keystonejs/app-graphql');
+const { AdminUIApp } = require('@keystonejs/app-admin-ui');
+const initialiseData = require('./initial-data');
+const { LocalFileAdapter } = require('@keystonejs/file-adapters');
+
+const { MongooseAdapter: Adapter } = require('@keystonejs/adapter-mongoose');
+
+const fileAdapter = new LocalFileAdapter({
+  src: './public/files',
+  path: '/files',
+});
+
+const PROJECT_NAME = 'tp-keystone';
+const adapterConfig = { mongoUri: 'mongodb://localhost/tp-keystone' };
+
+
+const keystone = new Keystone({
+  name: PROJECT_NAME,
+  adapter: new Adapter(adapterConfig),
+  onConnect: process.env.CREATE_TABLES !== 'true' && initialiseData,
+});
+
+// Access control functions
+const userIsAdmin = ({ authentication: { item: user } }) => Boolean(user && user.isAdmin);
+const userOwnsItem = ({ authentication: { item: user } }) => {
+  if (!user) {
+    return false;
+  }
+
+  // Instead of a boolean, you can return a GraphQL query:
+  // https://www.keystonejs.com/api/access-control#graphqlwhere
+  return { id: user.id };
+};
+
+const userIsAdminOrOwner = auth => {
+  const isAdmin = access.userIsAdmin(auth);
+  const isOwner = access.userOwnsItem(auth);
+  return isAdmin ? isAdmin : isOwner;
+};
+
+const userIsRedac = ({ authentication: { item: user } }) => Boolean(user && user.isRedac);
+const userIsWritter = ({ authentication: { item: user } }) => Boolean(user && user.isWritter);
+
+const access = { userIsAdmin, userOwnsItem, userIsAdminOrOwner, userIsRedac, userIsWritter };
+
+keystone.createList('User', {
+  fields: {
+    name: { type: Text },
+    email: {
+      type: Text,
+      isUnique: true,
+    },
+    isAdmin: {
+      type: Checkbox,
+      // Field-level access controls
+      // Here, we set more restrictive field access so a non-admin cannot make themselves admin.
+      access: {
+        update: access.userIsAdmin,
+      },
+    },
+    isRedac: {
+      type: Checkbox,
+      // Field-level access controls
+      // Here, we set more restrictive field access so a non-admin cannot make themselves admin.
+      access: {
+        update: access.userIsAdmin,
+      },
+    },
+    isWritter: {
+      type: Checkbox,
+      // Field-level access controls
+      // Here, we set more restrictive field access so a non-admin cannot make themselves admin.
+      access: {
+        update: access.userIsAdmin,
+      },
+    },
+    password: {
+      type: Password,
+    },
+  },
+  // List-level access controls
+  access: {
+    read: access.userIsAdminOrOwner,
+    update: access.userIsAdminOrOwner,
+    create: access.userIsAdmin,
+    delete: access.userIsAdmin,
+    auth: true,
+  },
+});
+
+keystone.createList('Article', {
+  fields: {
+    title: { type: Text },
+    resume: { type: Text },
+    image: { type: File, adapter: fileAdapter },
+    content: { type: Text },
+    category: {type: Relationship, ref: 'Category', schemaDoc: 'Réalisateur du film'},
+  },
+  // List-level access controls
+  access: {
+    read: access.userIsAdminOrOwner,
+    update: access.userIsAdminOrOwner,
+    create: access.userIsAdmin,
+    delete: access.userIsAdmin,
+    auth: true,
+  },
+});
+
+keystone.createList('Category', {
+  fields: {
+    name: { type: Text }
+  },
+  // List-level access controls
+  access: {
+    read: access.userIsAdminOrOwner,
+    update: access.userIsAdminOrOwner,
+    create: access.userIsAdmin,
+    delete: access.userIsAdmin,
+    auth: true,
+  },
+});
+
+const authStrategy = keystone.createAuthStrategy({
+  type: PasswordAuthStrategy,
+  list: 'User',
+});
+
+module.exports = {
+  keystone,
+  apps: [
+    new GraphQLApp(),
+    new AdminUIApp({
+      enableDefaultRoute: true,
+      authStrategy,
+    }),
+  ],
+};
